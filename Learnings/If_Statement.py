fb2b7ff933827e5964729fd4c2f596d81b67479c
Denis Deus <deusdenis40@gmail.com>
2026-03-10 22:06:47 +0300

    Add initial Python scripts for arithmetic operations, conditional statements, math functions, operator precedence, and string methods.

---
+++ Learnings/If_Statement.py
@@ -0,0 +1,7 @@
+is_hot = False
+
+if is_hot:
+    print("It's a hot day")
+else:
+    print("It's a cold day")
+print("Enjoy your day")
---
+++ Learnings/String_Method.py
@@ -0,0 +1,2 @@
+course = 'python for beginners'
+print('Python'in course)
