878e718f835b6da23abc191434510627dd34baea
Denis Deus <deusdenis40@gmail.com>
2026-05-09 20:44:31 +0300

    Add initial Python scripts for customer dictionary, tuples, conditions, unpacking, emoji converter, and number to word conversion.

---
+++ Learnings/Dictionaries.py
@@ -0,0 +1,7 @@
+customer = {
+    "name": "Denis Deus",
+    "age": "22",
+    "is_verified": True
+}
+
+print(customer.get("birthdate", "Nov 11th 2003"))
---
+++ Projects/Emoji_Converter.py
@@ -0,0 +1,13 @@
+message = input(">")
+words = message.split('   ')
+
+emojis = {
+    ":)": "Smile",
+    ":(": "Sad"
+}
+
+output =""
+
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
